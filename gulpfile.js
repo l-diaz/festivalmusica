369f75359d0c670875775368efefdf7bc97a80ea
l-diaz <lgquejada@gmail.com>
2026-04-05 16:47:06 -0500

    feat: update package dependencies and enhance image handling in gallery
    - Added 'glob', 'gulp-terser', and 'sharp' to devDependencies in package.json.
    - Refactored image creation in script.js to use <picture> element for better image format support (AVIF and WebP).
    - Updated image loading attributes for improved performance and accessibility.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,88 @@
-import { src, dest, watch, series } from 'gulp';
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
+import path from 'path'
+import fs from 'fs'
+import { glob } from 'glob'
+import { src, dest, watch, series } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
 
-const sass = gulpSass(dartSass);
+const sass = gulpSass(dartSass)
 
-export function js(done){
+import terser from 'gulp-terser'
+import sharp from 'sharp'
 
-    src('src/js/**/*.js', {sourcemaps: true})
-        .pipe(dest('build/js', {sourcemaps: true}))
-
-    done();
+export function js() {
+    return src('src/js/script.js')
+        .pipe(terser())
+        .pipe( dest('build/js') ) 
 }
 
-export function css( done ) {
-    src('src/scss/app.scss', {sourcemaps: true})
-        .pipe(sass().on('error', sass.logError))
-        .pipe(dest('build/css', {sourcemaps: true}))
-
-    done();
+export function css() {
+    return src('src/scss/app.scss', {sourcemaps: true})
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError) )
+        .pipe( dest('build/css', {sourcemaps: '.'}) )
 }
 
-export function dev(){
+export async function crop() {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        await Promise.all(images.map(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            return sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        }));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function imagenes() {
+    const srcDir = './src/img';
+    const buildDir = './build/img';
+    const images =  await glob('./src/img/**/*{jpg,png}')
+
+    await Promise.all(images.map(file => procesarImagenes(file, srcDir, buildDir)));
+}
+
+async function procesarImagenes(file, srcDir, buildDir) {
+    const relativePath = path.relative(srcDir, path.dirname(file));
+    const outputSubDir = path.join(buildDir, relativePath);
+    
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true })
+    }
+    const baseName = path.basename(file, path.extname(file))
+    const extName = path.extname(file)
+    const outputFile = path.join(outputSubDir, `${baseName}${extName}`)
+    const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`)
+    const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`)
+
+    const options = { quality: 80 }
+    await Promise.all([
+        sharp(file).jpeg(options).toFile(outputFile),
+        sharp(file).webp(options).toFile(outputFileWebp),
+        sharp(file).avif().toFile(outputFileAvif)
+    ])
+}
+
+export function dev() {
     watch('src/scss/**/*.scss', css)
     watch('src/js/**/*.js', js)
+    watch('src/img/**/*.{png,jpg}', imagenes)
 }
 
-export default series(js, css, dev)
+export default series( crop, js, css, imagenes, dev )
